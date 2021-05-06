9d333f0cbf2988ff9cde18fa61e5291720558d89
vakhtang.donadze <vakhtang.donadze@exactprosystems.com>
2021-05-06 11:31:03 +0400

    [TH2-1821] add LoggingConfigMap source hash in annotations

--- src/main/java/com/exactpro/th2/inframgr/initializer/LoggingConfigMap.java
+++ src/main/java/com/exactpro/th2/inframgr/initializer/LoggingConfigMap.java
@@ -25,7 +25,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.exactpro.th2.inframgr.k8s.K8sCustomResource.KEY_SOURCE_HASH;
 
 public class LoggingConfigMap {
 
@@ -108,6 +113,7 @@ public class LoggingConfigMap {
             cmData.put(ROOT_LOGGING_JSON_KEY, logLevelRoot + "\n");
 
             cm.setMetadata(Kubernetes.createMetadataWithAnnotation(configMapName, resourceLabel));
+            setSourceHash(cm.getMetadata().getAnnotations(), cmData);
             kube.createOrReplaceConfigMap(cm);
         } catch (Exception e) {
             logger.error("Exception copying \"{}\"", resourceLabel, e);
@@ -116,5 +122,24 @@ public class LoggingConfigMap {
 
     private static String getLoggingConfigMapName() throws IOException {
         return Config.getInstance().getKubernetes().getConfigMaps().get(LOGGING_CONFIGMAP_PARAM);
+    }
+
+    private static void setSourceHash(Map<String, String> annotations, Map<String, String> data) {
+        String dataStr = String.join(", ", data.values());
+        annotations.put(KEY_SOURCE_HASH, digest(dataStr));
+    }
+
+    private static String digest(String data) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] digest = md.digest(data.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte b : digest) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
