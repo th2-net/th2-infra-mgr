0f20f54c317c178c7589282d159d759ea6cde7b6
vakhtang.donadze <vakhtang.donadze@exactprosystems.com>
2021-05-20 18:17:14 +0400

    [TH2-1848] provide ability to define mq/grpc router and cradle configs for a namespace

--- src/main/java/com/exactpro/th2/inframgr/initializer/LoggingConfigMap.java
+++ src/main/java/com/exactpro/th2/inframgr/initializer/LoggingConfigMap.java
@@ -25,11 +25,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
-import static com.exactpro.th2.inframgr.k8s.K8sCustomResource.KEY_SOURCE_HASH;
+import static com.exactpro.th2.inframgr.util.SourceHashUtil.setSourceHash;
 
 public class LoggingConfigMap {
 
@@ -123,24 +121,5 @@ public class LoggingConfigMap {
 
     private static String getLoggingConfigMapName() throws IOException {
         return Config.getInstance().getKubernetes().getConfigMaps().get(LOGGING_CONFIGMAP_PARAM);
-    }
-
-    private static void setSourceHash(Map<String, String> annotations, Map<String, String> data) {
-        String dataStr = String.join(", ", data.values());
-        annotations.put(KEY_SOURCE_HASH, digest(dataStr));
-    }
-
-    private static String digest(String data) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] digest = md.digest(data.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for (byte b : digest) {
-                sb.append(String.format("%02x", b));
-            }
-            return sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
