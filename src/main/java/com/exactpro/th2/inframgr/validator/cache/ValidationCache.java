5c190a0f8fbd40b5164cd842455cb85169215b37
vakhtang.donadze <vakhtang.donadze@exactprosystems.com>
2021-10-07 16:18:15 +0400

    [TH2-2489] update dependencies

--- src/main/java/com/exactpro/th2/inframgr/validator/cache/ValidationCache.java
+++ src/main/java/com/exactpro/th2/inframgr/validator/cache/ValidationCache.java
@@ -1,10 +1,10 @@
 package com.exactpro.th2.inframgr.validator.cache;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ValidationCache {
-    private static final Map<String, SchemaValidationTable> validationTable = new HashMap<>();
+    private static final Map<String, SchemaValidationTable> validationTable = new ConcurrentHashMap<>();
 
     public static SchemaValidationTable getSchemaTable(String schemaName) {
         return validationTable.computeIfAbsent(schemaName, k -> new SchemaValidationTable());
